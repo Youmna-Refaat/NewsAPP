72d0eb44af3c414cf660918479e9220b00d5d7de
YoumnaRefaat <youmnarefaat884@gmail.com>
2024-10-05 21:04:13 +0300

    modified main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:news_app/views/home_view.dart';
 
 void main() {
   runApp(const NewsApp());
 }
+
+final Dio dio = Dio();
 
 class NewsApp extends StatelessWidget {
   const NewsApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
   }
 }
